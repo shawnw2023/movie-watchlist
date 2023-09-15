ea15266d21f2042d2ceecea17c62d0e5d6926481
Shawn Wagner <Shawnwagner2814@gmail.com>
2023-09-15 18:30:31 -0400

    finished watchlist

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <h2>Shawn's Movie App</h2>
+      </header>
+    </div>
+  )
+}
+
+export default Header
